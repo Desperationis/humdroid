3e3c2d7e4cc011febd7e6b566f3fe9f8bf85e248
Desperationis <smarttdiego@gmail.com>
2022-09-16 15:12:26 -0700

    Add IPC class for IPC.py

--- scrcpy/IPC.py
+++ scrcpy/IPC.py
@@ -1,12 +1,27 @@
 import socket
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+class IPCSocket:
+    UDP_IP = "127.0.0.1"
+    UDP_PORT = 5005
 
-# UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
+    def __init__(self, isServer=False):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        if isServer:
+            self.sock.bind((self.UDP_IP, self.UDP_PORT))
+
+    def receive(self):
+        data,addr = self.sock.recvfrom(1024)
+        return data
+
+    def send(self, data):
+        self.sock.sendto(data, (self.UDP_IP, self.UDP_PORT))
+
+ipc = IPCSocket()
+ipc.send(b"Hello, world!")
 
 while True:
-    data,addr = sock.recvfrom(1024)
-    print("Received message: %s" % data)
+    print(ipc.receive())
+
+
+
