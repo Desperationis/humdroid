2de7a7c05e5ef8dbfb6d0cdf6bb10fa4cd795c36
Desperationis <smarttdiego@gmail.com>
2022-09-17 20:06:39 -0700

    testTransmitter.py Add code to test output received

--- scrcpy/testTransmitter.py
+++ scrcpy/testTransmitter.py
@@ -2,6 +2,7 @@ from IPC import IPCSocket
 import json
 from os import listdir, walk
 from os.path import isfile, join
+import time
 
 data = {
     "loadTemplates" : {
@@ -28,8 +29,24 @@ sock = IPCSocket()
 
 sock.send(bytearray(jsonData))
 
+
+for i in range(100):
+    compareData = {
+        "compareSingle" : {
+            "photo" : "/home/adhoc/Desktop/humdroid/opencv/executable/background.jpg"
+        }
+    }
+    compareJSON = json.dumps(compareData).encode("UTF-8")
+    sock.send(bytearray(compareJSON))
+
+    print("Received: %s" % sock.receive())
+
+
+
+"""
 received = sock.receive()
 receivedJSON = json.loads(received)
 print("Received message: %s" % received)
 print("Parameter: %s" % receivedJSON["loadTemplates"]["templates"])
+"""
 
